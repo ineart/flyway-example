69a70465de0b9f66c0dbb0c5e7ba3c68b13b86cc
ineart <registration.rt@t-online.de>
2018-10-27 12:27:37 +0200

    Initial commit

---
+++ src/main/java/de/ineart/flyway_migration_example/App.java
@@ -0,0 +1,13 @@
+package de.ineart.flyway_migration_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
